dd03d7e0766ea3e8075c72c13576b5aee6c1eab7
ThirukumaranSelva <thirukumaran@codecraft.co.in>
2023-11-20 10:18:01 +0530

    Nov 20th Code

--- SeleniumAssignment/src/test/java/com/qa/seleniumAssignment/NaturesBasket.java
+++ SeleniumAssignment/src/test/java/com/qa/seleniumAssignment/NaturesBasket.java
@@ -122,9 +122,6 @@ public class NaturesBasket {
         wait.until(ExpectedConditions.visibilityOf(emailID));
         actions.click(emailID).sendKeys(name).build().perform();
         actions.keyDown(emailID,Keys.CONTROL).sendKeys("ac").keyUp(Keys.CONTROL).build().perform();
-        emailID.clear();
-        actions.click(emailID).sendKeys("Test@gmail.com").build().perform();
-
 
         String date=LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss"));
 
